4445e9e03c7805430d4aed7be64e9884f97ef8aa
zoomkoding <zoomkoding@gmail.com>
2020-12-27 00:06:46 +0900

    feat: create constants for backend

--- server/constants.js
+++ server/constants.js
@@ -7,3 +7,21 @@ exports.HTTP_STATUS = {
   DUPLICATED: 409,
   SERVER_ERROR: 500,
 };
+
+exports.LECTURE_COLUMNS = {
+  gubun: '구      분',
+  code: '과목코드',
+  bunban: '분      반',
+  name: '과목명\n      (CourseName)',
+  credit: '학점',
+  info: '개설정보',
+  period: '시간\n      (Period)',
+  roomNo: '강의실',
+  maxNum: '정원',
+  curNum: '인원',
+  english: '영어',
+  gyoyang: '교양\n      실무',
+  grading: '성적유형',
+  pfPossible: 'PF병행',
+  plan: '강의\n\t  계획서',
+};
