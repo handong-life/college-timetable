1cad09d6851b108c3bdf636fd64574b287ff7d1a
zoomkoding <zoomkoding@gmail.com>
2020-12-28 02:14:54 +0900

    update: update SearchBar
    - margin bottom
    - shadow when hovered

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './style.scss';
 
 export default function SearchBar({ initialSearch = '', onSearchSubmit }) {
---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Timetable } from './Timetable';
+export { default as SearchBar } from './SearchBar';
+export { default as LectureCard } from './LectureCard';
