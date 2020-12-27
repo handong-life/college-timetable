2e052333cd87c70f2c781e3570dc6788895e6e6c
zoomkoding <zoomkoding@gmail.com>
2020-12-28 02:12:53 +0900

    feat: create routers for bookmarks

--- server/routes/lecture.js
+++ server/routes/lecture.js
@@ -3,5 +3,8 @@ const router = Router();
 const lectureController = require('../controllers/lecture');
 
 router.get('/search', lectureController.getSearchResults);
+router.get('/bookmark', lectureController.getBookmarks);
+router.post('/bookmark/:lectureId', lectureController.bookmarkLecture);
+router.delete('/unbookmark/:lectureId', lectureController.unbookmarkLecture);
 
 module.exports = router;
