62f91d118e11c889727830c41367347bb1ef7b2c
zoomkoding <zoomkoding@gmail.com>
2020-12-28 02:15:28 +0900

    feat: create APIs for bookmarks

--- server/controllers/lecture.js
+++ server/controllers/lecture.js
@@ -1,13 +1,45 @@
 const Lecture = require('../models/lecture');
+const User = require('../models/user');
+const UserLectureRelation = require('../models/user_lecture_relation');
 const { searchWhereClause } = require('../utils/query_helper');
 
 exports.getSearchResults = async (req, res) => {
   const { search } = req.query;
-  console.log(decodeURIComponent(search));
   const searchResults = await Lecture.findAll({
     where: searchWhereClause(decodeURIComponent(search)),
     limit: 10,
   });
-  console.log(searchResults);
   res.send(searchResults);
+};
+
+exports.bookmarkLecture = async (req, res) => {
+  await UserLectureRelation.create({
+    userId: req.user.id,
+    lectureId: +req.params.lectureId,
+  });
+  res.send('complete');
+};
+
+exports.unbookmarkLecture = async (req, res) => {
+  await UserLectureRelation.destroy({
+    where: {
+      userId: req.user.id,
+      lectureId: +req.params.lectureId,
+    },
+  });
+  res.send('complete');
+};
+
+exports.getBookmarks = async (req, res) => {
+  const userBookmarks = await User.findOne({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      model: Lecture,
+      through: UserLectureRelation,
+    },
+  });
+
+  res.send(userBookmarks.lectures);
 };
