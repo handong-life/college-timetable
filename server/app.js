8d7727b1951723f3dfa2a049111c7ca7f7490be9
zoomkoding <zoomkoding@gmail.com>
2021-01-07 01:24:20 +0900

    chore: delete the view engine for the server

--- server/app.js
+++ server/app.js
@@ -19,11 +19,6 @@ sequelize.sync({ force: false });
 
 const app = express();
 const router = require('./routes');
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -54,10 +49,9 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err);
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
