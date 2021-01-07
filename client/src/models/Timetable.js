5bc73c8e224d3cbbbba4402a666c2b5ab8204bc7
zoomkoding <zoomkoding@gmail.com>
2021-01-08 02:52:10 +0900

    [#11] fix : fix no Timetable problem

--- client/src/models/Timetable.js
+++ client/src/models/Timetable.js
@@ -1,7 +1,9 @@
+import { Lecture } from '.';
+
 export default class Timetable {
   constructor(raw) {
     this.id = raw.id;
     this.title = raw.title;
-    this.lectures = raw.lectures;
+    this.lectures = raw.lectures ? raw.lectures.map((lecture) => new Lecture(lecture)) : [];
   }
 }
