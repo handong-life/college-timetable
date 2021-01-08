626008fb2056a57889168164065dbfc77a2daa99
zoomkoding <zoomkoding@gmail.com>
2021-01-09 05:12:04 +0900

    feat: create feedback rounter

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,7 @@ const router = Router();
 const userController = require('../controllers/user');
 
 router.get('/', userController.getUser);
+router.post('/feedback', userController.createFeedback);
 router.get('/bookmark', userController.getBookmarks);
 router.post('/bookmark/:lectureId', userController.bookmarkLecture);
 router.delete('/bookmark/:lectureId', userController.unbookmarkLecture);
