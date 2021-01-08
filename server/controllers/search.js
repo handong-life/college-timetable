650355ccce587f70475ee10fea9690c294344196
zoomkoding <zoomkoding@gmail.com>
2021-01-09 04:55:23 +0900

    feat: save search results

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,11 +1,13 @@
 const Lecture = require('../models/lecture');
+const Search = require('../models/search');
 const { searchWhereClause } = require('../utils/query_helper');
 
 exports.getSearchResults = async (req, res) => {
-  const { search } = req.query;
+  const search = decodeURIComponent(req.query.search);
+  Search.create({ userId: req.user.id, search });
   const searchResults = await Lecture.findAll({
-    where: searchWhereClause(decodeURIComponent(search)),
-    limit: 10,
+    where: searchWhereClause(search),
+    limit: 30,
   });
   res.send(searchResults);
 };
