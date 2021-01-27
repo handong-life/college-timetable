9c92a685a56ca1ec47436b7933948c543e4c72e4
zoomkoding <zoomkoding@gmail.com>
2021-01-27 22:58:50 +0900

    feat: create user related APIs and add redefine model
    - make user related APIs handled in User model
    - add timetables and bookmarks to User model

--- client/src/models/User.js
+++ client/src/models/User.js
@@ -1,6 +1,17 @@
+import { Axios } from '../lib/axios';
+import { Timetable, BookmarkedLecture } from '.';
+
 export default class User {
   constructor(raw) {
     this.id = raw.id;
     this.email = raw.email;
+    this.timetables = raw.timetables.map((timetable) => new Timetable(timetable, raw.lectures));
+    this.bookmarks = raw.lectures.map((lecture) => new BookmarkedLecture(lecture));
   }
+
+  static getUser = async () => await Axios().get(`/user`);
+  static getAuth = async () => await Axios().get('/auth');
+  static reportFeedback = async (feedback) => await Axios().post(`/user/feedback`, { feedback });
+  static bookmarkLecture = async (id) => await Axios().post(`/user/bookmark/${id}`);
+  static unbookmarkLecture = async (id) => await Axios().delete(`/user/bookmark/${id}`);
 }
