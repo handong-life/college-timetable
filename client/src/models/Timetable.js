04f45ceb800a1c070c940f7e44f73bc4f3cf518a
zoomkoding <zoomkoding@gmail.com>
2021-01-27 23:02:25 +0900

    feat: move Timetable related APIs under Timetable model

--- client/src/models/Timetable.js
+++ client/src/models/Timetable.js
@@ -1,9 +1,21 @@
-import { Lecture } from '.';
+import { Axios } from '../lib/axios';
+import { TimetableLecture } from './Lecture';
 
 export default class Timetable {
-  constructor(raw) {
+  constructor(raw, bookmarks) {
     this.id = raw.id;
     this.title = raw.title;
-    this.lectures = raw.lectures ? raw.lectures.map((lecture) => new Lecture(lecture)) : [];
+    this.lectures = raw.lectures
+      ? raw.lectures.map((lecture) => new TimetableLecture(lecture, bookmarks))
+      : [];
   }
+
+  static create = async (title) => await Axios().post(`/timetable`, { title });
+  static updateTitle = async (id, title) => await Axios().put(`/timetable`, { id, title });
+  static delete = async (id) => await Axios().delete(`/timetable/${id}`);
+  static getBySharedId = async (sharedId) => await Axios().get(`/share/${sharedId}`);
+  static addLecture = async (timetableId, lectureId) =>
+    await Axios().post(`/timetable/lecture/${timetableId}/${lectureId}`);
+  static deleteLecture = async (timetableId, lectureId) =>
+    await Axios().delete(`/timetable/lecture/${timetableId}/${lectureId}`);
 }
