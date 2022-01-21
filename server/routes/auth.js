4194cd7a17961915c86626aa8efbf48f1516c577
zoomkoding <zoomkoding@gmail.com>
2022-01-22 00:27:52 +0900

    fix: cookie를 전달하기 위해 client domain도 전달

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -18,7 +18,9 @@ router.get(
   passport.authenticate('google', { failureRedirect: '/auth', session: false }),
   function (req, res) {
     const accessToken = JWT.sign({ userId: req.user.id }, process.env.JWT_SECRET);
-    res.cookie('accessToken', accessToken).redirect(process.env.GOOGLE_REDIRECT_URL);
+    res
+      .cookie('accessToken', accessToken, { domain: process.env.CLIENT_DOMAIN })
+      .redirect(process.env.GOOGLE_REDIRECT_URL);
   },
 );
 
