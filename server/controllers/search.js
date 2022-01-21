31c1f1c03a37a79acd0f53dcf4ec83ace4511293
junglesub <junglesubmarine@gmail.com>
2022-01-21 15:03:34 +0900

    feat: give add/bookmark/spike count when searching

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,5 +1,8 @@
 const Lecture = require('../models/lecture');
 const Search = require('../models/search');
+const Timetable = require('../models/timetable');
+const UserLectureGleaningRelation = require('../models/user_lecture_gleaning_relation');
+const UserLectureRelation = require('../models/user_lecture_relation');
 const { searchWhereClause } = require('../utils/query_helper');
 
 exports.getSearchResults = async (req, res) => {
@@ -14,5 +17,44 @@ exports.getSearchResults = async (req, res) => {
     offset: page ? limit * (+page - 1) : 0,
   });
 
-  res.send({ pages: count === 0 ? 0 : Math.ceil(count / limit), lectures });
+  // Get spike information
+  const lecturesWithCount = await Promise.all(
+    lectures.map(
+      async (lec) =>
+        new Promise(async (res) => {
+          const [add, bookmark, spike] = await Promise.all([
+            // Add
+            Timetable.count({
+              include: {
+                model: Lecture,
+                where: {
+                  id: lec.id,
+                },
+              },
+              distinct: true,
+              col: 'userId',
+            }),
+            // Bookmark
+            UserLectureRelation.count({
+              where: { lectureId: lec.id },
+            }),
+            // Spike
+            UserLectureGleaningRelation.count({
+              where: { lectureId: lec.id },
+            }),
+          ]);
+
+          return res({
+            ...lec.dataValues,
+            count: {
+              add,
+              bookmark,
+              spike,
+            },
+          });
+        }),
+    ),
+  );
+
+  res.send({ pages: count === 0 ? 0 : Math.ceil(count / limit), lectures: lecturesWithCount });
 };
